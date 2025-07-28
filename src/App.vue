2f0a1d8c6108396561b1cc3fe6d07c7c9a93cfac
Thomas Joy <thomaspjoy1@gmail.com>
2025-07-28 15:52:00 +0100

    custom tailwind config and themes

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@ function updateCart(id) {
 </script>
 
 <template>
-  <div class="h-[60px] mb-[25px] bg-gradient-to-r from-[#16c0b0] to-[#84cf6a]"></div>
+  <div class="h-[60px] mb-[25px] bg-gradient-to-r from-ocean to-leaf"></div>
 
-  <div class="my-[25px] mx-[100px] p-[30px] border border-solid border-[#d8d8d8] bg-white float-right">Cart({{ cart.length }})</div>
+  <div class="my-[25px] mx-[100px] p-[30px] border border-solid border-mist bg-cloud float-right">Cart({{ cart.length }})</div>
   <product-display :premium="premium" @add-to-cart="updateCart"></product-display>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ module.exports = {
     "index.html"
   ],
   theme: {
-    extend: {},
+    colors: {
+        'ocean': '#16c0b0',
+        'leaf': '#84cf6a',
+        'mist': '#d8d8d8',
+        'midnight': '#39495c',
+        'cloud': '#ffffff'
+      },
+      screens: {
+        'sm': '660px',
+        'md': '860px',
+        'lg': '1060px',
+        'xl': '1260px',
+        '2xl': '1460px'
+      }
   },
-  plugins: [],
+  plugins: []
 }
 
